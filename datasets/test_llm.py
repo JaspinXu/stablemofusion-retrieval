4ea357dbc510afce97fe122ca166b646eac7a1d7
JaspinXu <111742154+JaspinXu@users.noreply.github.com>
2025-02-17 23:30:55 +0800

    Update test_llm.py

--- datasets/test_llm.py
+++ datasets/test_llm.py
@@ -4,8 +4,7 @@ import json
 import os
 import time
 
-client = OpenAI(api_key='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')
-
+client = OpenAI(api_key=''
 def get_local_text_from_text(text_path, out_local_text_path, prompt_path):
     try:
         with open(prompt_path, 'r') as f:
